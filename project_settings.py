Fixed repeatability so there will be no overfitting. Now we learn the threshold so there is no information leakage

project_settings.py
@@ -43,13 +43,14 @@ class ProSet:
         self.proper = True
         self.remove_mean = False
         self.mu = None
-        self.test_mode = False
+        self.test_mode = True
         self.val_size = 0.2
         self.seed = 42
         self.r = None
         self.samp_per_id = 60
         self.human_flag = False
         self.wandb_enable = True
+        self.rearrange = False  # (when false is real training and when true it means train on all mice and test on unseen windows)
         # early stopping:
         self.patience = 5
         self.lr_ker_size = 5
@@ -123,7 +124,7 @@ class HRVDataset(Dataset):
         self.x = x.copy()
         self.y = y.copy()
         self.p = p
-        np.random.seed(p.seed)
+        # np.random.seed(p.seed)
         self.r = np.random.randint(0, 2, x.shape[0])
         self.mode = mode
 
@@ -144,7 +145,7 @@ class HRVDataset(Dataset):
         if self.mode:
             y = self.y[idx, 0]
             r = self.r[idx]  # 50%-50%
-            np.random.seed(self.p.seed)
+            # np.random.seed(self.p.seed)
             if r:  # find negative example
                 neg_list = np.argwhere(self.y[:, 0] != y)
                 idx = neg_list[np.random.randint(0, len(neg_list))].item()

single_model_functions.py
@@ -656,49 +656,111 @@ def calc_metric(scores_list, y_list, epoch, p, train_mode='Training'):
     res_orig = 2 * scores - 1
     res_orig = res_orig.cpu().detach()
     y = y.cpu().detach()
-    thresh = torch.linspace(scores.min().item() - 1, scores.max().item() + 1, 100)
-    far = torch.zeros_like(thresh)  # fpr
-    frr = torch.zeros_like(thresh)  # 1 - tpr
-    acc = torch.zeros_like(thresh)
-    conf_mat_tensor = torch.zeros((thresh.shape[0], 2, 2))
-    for idx, trh in enumerate(thresh):
-        res2 = torch.clone(res_orig)
-        conf_mat = torch.zeros((2, 2))
-        res2[res_orig >= trh] = 1
-        res2[res_orig < trh] = 0
-        conf = (res2 == y)
-        conf_mat[0, 0] += torch.sum(1 * (conf[res2 == 0] == 1))
-        conf_mat[0, 1] += torch.sum(1 * (conf[res2 == 1] == 0))
-        conf_mat[1, 0] += torch.sum(1 * (conf[res2 == 0] == 0))
-        conf_mat[1, 1] += torch.sum(1 * (conf[res2 == 1] == 1))
-        far[idx] = conf_mat[0, 1] / conf_mat.sum(axis=1)[0]
-        frr[idx] = 1 - (conf_mat[1, 1] / conf_mat.sum(axis=1)[1])
-        acc[idx] = conf_mat.trace() / conf_mat.sum()
-        conf_mat_tensor[idx, :, :] = conf_mat
-    far[torch.isnan(far)] = 1
-    frr[torch.isnan(frr)] = 1
-    acc[torch.isnan(acc)] = 0
-    err_idx = np.argmin(np.abs(frr - far))
-    err = 0.5 * (frr[err_idx] + far[err_idx])
-    if p.wandb_enable:
-        # if train_mode == 'Training':
-        #     wandb.log({'training_thresh_err': thresh[err_idx]})
-        # else:
-        #     wandb.log({'testing_thresh_err': thresh[err_idx]})
-        pass
-    acc_idx = torch.argmax(acc)
-    best_acc = acc[acc_idx]
-    print('With threshold of {:.2f} we got minimal ERR of {:.2f} and accuracy of  {:.2f}'.format(thresh[err_idx], err,
-                                                                                                 acc[err_idx]))
-    print(conf_mat_tensor[err_idx])
-    print('With threshold of {:.2f} we got maximal accuracy of {:.2f} and ERR of  {:.2f}'.format(thresh[acc_idx],
-                                                                                                 best_acc, 0.5 * (frr[
-                                                                                                                      acc_idx] +
-                                                                                                                  far[
-                                                                                                                      acc_idx])))
-    best_th = thresh[acc_idx]
+    if train_mode == 'Training':
+        thresh = torch.linspace(scores.min().item() - 1, scores.max().item() + 1, 100)
+        far = torch.zeros_like(thresh)  # fpr
+        frr = torch.zeros_like(thresh)  # 1 - tpr
+        acc = torch.zeros_like(thresh)
+        F1 = torch.zeros_like(thresh)
+        conf_mat_tensor = torch.zeros((thresh.shape[0], 2, 2))
+        for idx, trh in enumerate(thresh):
+            res2 = torch.clone(res_orig)
+            conf_mat = torch.zeros((2, 2))
+            res2[res_orig >= trh] = 1
+            res2[res_orig < trh] = 0
+            conf = (res2 == y)
+            conf_mat[0, 0] += torch.sum(1 * (conf[res2 == 0] == 1))
+            conf_mat[0, 1] += torch.sum(1 * (conf[res2 == 1] == 0))
+            conf_mat[1, 0] += torch.sum(1 * (conf[res2 == 0] == 0))
+            conf_mat[1, 1] += torch.sum(1 * (conf[res2 == 1] == 1))
+            far[idx] = conf_mat[0, 1] / conf_mat.sum(axis=1)[0]
+            frr[idx] = 1 - (conf_mat[1, 1] / conf_mat.sum(axis=1)[1])
+            acc[idx] = conf_mat.trace() / conf_mat.sum()
+            PPV = conf_mat[1, 1]/(conf_mat.sum(axis=0)[1])
+            Se = conf_mat[1, 1]/(conf_mat.sum(axis=1)[1])
+            F1[idx] = 2*(PPV*Se)/(PPV+Se)
+            conf_mat_tensor[idx, :, :] = conf_mat
+        far[torch.isnan(far)] = 1
+        frr[torch.isnan(frr)] = 1
+        acc[torch.isnan(acc)] = 0
+        F1[torch.isnan(F1)] = 0
+        err_idx = np.argmin(np.abs(frr - far))
+        err = 0.5 * (frr[err_idx] + far[err_idx])
+        if p.wandb_enable:
+            # if train_mode == 'Training':
+            #     wandb.log({'training_thresh_err': thresh[err_idx]})
+            # else:
+            #     wandb.log({'testing_thresh_err': thresh[err_idx]})
+            pass
+        acc_idx = torch.argmax(acc)
+        best_acc = acc[acc_idx]
+        F1_idx = torch.argmax(F1)
+        best_F1 = F1[F1_idx]
+        with open(p.log_path + '/thresh.pkl', 'wb') as f:
+            pickle.dump([thresh[err_idx], thresh[acc_idx], thresh[F1_idx], err_idx, acc_idx, F1_idx], f)
+        print('With ERR threshold of {:.2f} we got maximal accuracy of {:.2f} and maximal F1 of {:.2f}'
+              .format(thresh[err_idx], acc[err_idx], F1[err_idx]))
+        print(conf_mat_tensor[err_idx])
+        print('With ACC threshold of {:.2f} we got maximal accuracy of {:.2f} and maximal F1 of {:.2f}'
+              .format(thresh[acc_idx], acc[acc_idx], F1[acc_idx]))
+        best_th = thresh[acc_idx]
+        print(conf_mat_tensor[acc_idx])
+        print('With F1 threshold of {:.2f} we got maximal accuracy of {:.2f} and maximal F1 of {:.2f}'
+              .format(thresh[F1_idx], acc[F1_idx], F1[F1_idx]))
+        print(conf_mat_tensor[F1_idx])
 
-    print(conf_mat_tensor[acc_idx])
+            # if epoch == p.num_epochs:
+
+    else:
+        with open(p.log_path + '/thresh.pkl', 'rb') as f:
+            th_err, th_acc, th_f1, err_idx, acc_idx, F1_idx = pickle.load(f)
+            conf_mat_tensor = torch.zeros((3, 2, 2))
+            stat_res = torch.zeros((3, 2))  # three rows for different types of thresh and two columns for the results (acc and F1)
+            th_list = [th_err, th_acc, th_f1]
+            for idx, trh in enumerate(th_list):
+                res2 = torch.clone(res_orig)
+                conf_mat = torch.zeros((2, 2))
+                res2[res_orig >= trh] = 1
+                res2[res_orig < trh] = 0
+                conf = (res2 == y)
+                conf_mat[0, 0] += torch.sum(1 * (conf[res2 == 0] == 1))
+                conf_mat[0, 1] += torch.sum(1 * (conf[res2 == 1] == 0))
+                conf_mat[1, 0] += torch.sum(1 * (conf[res2 == 0] == 0))
+                conf_mat[1, 1] += torch.sum(1 * (conf[res2 == 1] == 1))
+                far = conf_mat[0, 1] / conf_mat.sum(axis=1)[0]
+                frr = 1 - (conf_mat[1, 1] / conf_mat.sum(axis=1)[1])
+                acc = conf_mat.trace() / conf_mat.sum()
+                PPV = conf_mat[1, 1] / (conf_mat.sum(axis=0)[1])
+                Se = conf_mat[1, 1] / (conf_mat.sum(axis=1)[1])
+                F1 = 2 * (PPV * Se) / (PPV + Se)
+                conf_mat_tensor[idx, :, :] = conf_mat
+                far[torch.isnan(far)] = 1
+                frr[torch.isnan(frr)] = 1
+                acc[torch.isnan(acc)] = 0
+                F1[torch.isnan(F1)] = 0
+                stat_res[idx, 0] = acc
+                stat_res[idx, 1] = F1
+                err = 0.5*(far + frr)
+                best_acc = acc
+                if p.wandb_enable:
+                    # if train_mode == 'Training':
+                    #     wandb.log({'training_thresh_err': thresh[err_idx]})
+                    # else:
+                    #     wandb.log({'testing_thresh_err': thresh[err_idx]})
+                    pass
+
+        print('With ERR threshold of {:.2f} we got maximal accuracy of {:.2f} and maximal F1 of {:.2f}'
+              .format(th_err, stat_res[0, 0], stat_res[0, 1]))
+        print(conf_mat_tensor[0])
+        print('With ACC threshold of {:.2f} we got maximal accuracy of {:.2f} and maximal F1 of {:.2f}'
+              .format(th_acc, stat_res[1, 0], stat_res[1, 1]))
+        print(conf_mat_tensor[1])
+        print('With F1 threshold of {:.2f} we got maximal accuracy of {:.2f} and maximal F1 of {:.2f}'
+              .format(th_f1, stat_res[2, 0], stat_res[2, 1]))
+        print(conf_mat_tensor[2])
+        ii = torch.argmax(stat_res[:, 1])
+        best_th = th_list[ii]  # meaning gave the best F1 on test
+
 
     if epoch < 10:
         str_epoch = '0' + str(epoch)
@@ -707,38 +769,45 @@ def calc_metric(scores_list, y_list, epoch, p, train_mode='Training'):
     if train_mode == 'Training':
         name1 = 'conf_err_train_epoch_'
         name2 = 'conf_acc_train_epoch_'
+        if epoch == p.num_epochs:
+            torch.save(conf_mat_tensor[err_idx], p.log_path + '/' + name1 + str_epoch + '.pt')
+            torch.save(conf_mat_tensor[acc_idx], p.log_path + '/' + name2 + str_epoch + '.pt')
+            res2 = torch.clone(res_orig)
+            # paint orange res2[res2>= thresh[err_idx]] and blue res2[res < thresh[err_idx]], x_axis is res2 itself. add dashed
+            # line of optimal threshold. Do the same for accuracy, do it wuth subplot
+            sns.histplot(x=res2, hue=y, bins=50)  # , stat='probability', bins=int(np.ceil(0.4*len(y))))
+            plt.axvline(x=thresh[acc_idx], color='r', linestyle='dashed')
+            name1 = '/histo_train_epoch_'
+            name2 = '/err_acc_train_epoch_'
+            plt.xlabel('Cosine similarity [N.U]')
+            plt.title('{} mode: ACC = {:.2f}%, tr = {:.2f}'.format(train_mode, 100 * best_acc, thresh[acc_idx]))
+            plt.savefig(p.log_path + name1 + str_epoch + '.png')
+            plt.close()
+            # if np.mod(epoch, 10) == 0:
+            plt.plot(thresh, far, thresh, frr)  # , thresh, acc)
+            plt.legend(['FAR', 'FRR'])  # , 'ACC'])
+            # plt.title('{} mode: ERR = {:.2f}%, tr = {:.2f}, ACC = {:.2f}%, tr = {:.2f}'.format(train_mode, 100*err, thresh[err_idx], 100*best_acc, thresh[acc_idx]))
+            plt.xlabel('Cosine similarity [N.U]')
+            plt.ylabel('Error [N.U]')
+            plt.title('{} mode: ERR = {:.2f}%, tr = {:.2f}'.format(train_mode, 100 * err, thresh[err_idx]))
+            plt.savefig(p.log_path + name2 + str_epoch + '.png')
+            plt.close()
     else:
         name1 = 'conf_err_val_epoch_'
         name2 = 'conf_acc_val_epoch_'
-    if epoch == p.num_epochs:
-        torch.save(conf_mat_tensor[err_idx], p.log_path + '/' + name1 + str_epoch + '.pt')
-        torch.save(conf_mat_tensor[acc_idx], p.log_path + '/' + name2 + str_epoch + '.pt')
-        res2 = torch.clone(res_orig)
-        # paint orange res2[res2>= thresh[err_idx]] and blue res2[res < thresh[err_idx]], x_axis is res2 itself. add dashed
-        # line of optimal threshold. Do the same for accuracy, do it wuth subplot
-        sns.histplot(x=res2, hue=y, bins=50)  # , stat='probability', bins=int(np.ceil(0.4*len(y))))
-        plt.axvline(x=thresh[acc_idx], color='r', linestyle='dashed')
-        # plt.plot(thresh[err_idx] * np.ones(20), np.linspace(0, 0.1, 20), thresh[acc_idx] * np.ones(20), np.linspace(0, 0.1, 20))
-        if train_mode == 'Training':
-            name1 = '/histo_train_epoch_'
-            name2 = '/err_acc_train_epoch_'
-        else:
+        if epoch == p.num_epochs:
+            torch.save(conf_mat_tensor[0], p.log_path + '/' + name1 + str_epoch + '.pt')
+            torch.save(conf_mat_tensor[1], p.log_path + '/' + name2 + str_epoch + '.pt')
+            res2 = torch.clone(res_orig)
+            sns.histplot(x=res2, hue=y, bins=50)  # , stat='probability', bins=int(np.ceil(0.4*len(y))))
+            plt.axvline(x=th_acc, color='r', linestyle='dashed')
+            # plt.plot(thresh[err_idx] * np.ones(20), np.linspace(0, 0.1, 20), thresh[acc_idx] * np.ones(20), np.linspace(0, 0.1, 20))
             name1 = '/histo_val_epoch_'
             name2 = '/err_val_epoch_'
-        plt.xlabel('Cosine similarity [N.U]')
-        plt.title('{} mode: ACC = {:.2f}%, tr = {:.2f}'.format(train_mode, 100 * best_acc, thresh[acc_idx]))
-        plt.savefig(p.log_path + name1 + str_epoch + '.png')
-        plt.close()
-        # if np.mod(epoch, 10) == 0:
-        plt.plot(thresh, far, thresh, frr)  # , thresh, acc)
-        plt.legend(['FAR', 'FRR'])  # , 'ACC'])
-        # plt.title('{} mode: ERR = {:.2f}%, tr = {:.2f}, ACC = {:.2f}%, tr = {:.2f}'.format(train_mode, 100*err, thresh[err_idx], 100*best_acc, thresh[acc_idx]))
-        plt.xlabel('Cosine similarity [N.U]')
-        plt.ylabel('Error [N.U]')
-        plt.title('{} mode: ERR = {:.2f}%, tr = {:.2f}'.format(train_mode, 100 * err, thresh[err_idx]))
-        plt.savefig(p.log_path + name2 + str_epoch + '.png')
-        plt.close()
-        # plt.show()
+            plt.xlabel('Cosine similarity [N.U]')
+            plt.title('{} mode: ACC = {:.2f}%, tr = {:.2f}'.format(train_mode, 100 * best_acc, th_acc))
+            plt.savefig(p.log_path + name1 + str_epoch + '.png')
+            plt.close()
     return err, best_acc, best_th.to(scores.device), y.to(scores.device)
 
 

wandb_main.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     command = "nvidia-smi --query-gpu=memory.free --format=csv"
     memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
     memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
-    device_list = [0,1,2,3,4,5,6,7]
+    device_list = [0, 1, 2, 3, 4, 5, 6, 7]
     for device in device_list:
         if memory_free_values[device] > 4000:
             p.device = device
@@ -42,7 +42,12 @@ if __name__ == '__main__':
     #     p = argparse.Namespace(**wandb.config)
     p.device = device
     p.wandb_enable = False
-    p.test_mode = True
+    # p.test_mode = True
+    np.random.seed(p.seed)
+    torch.manual_seed(p.seed)
+    torch.cuda.manual_seed_all(p.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     if not(p.test_mode):
         print('Med mode is : {}'.format(p.med_mode))
         if p.human_flag:
@@ -54,17 +59,21 @@ if __name__ == '__main__':
         tr_dataset_1 = load_datasets(p.train_path, p, human_flag=p.human_flag, samp_per_id=p.samp_per_id)
         x_tr, y_tr, x_val, y_val = split_dataset(tr_dataset_1, p)
         tr_dataset_1, val_dataset_1, scaler1 = scale_dataset(p, x_tr, y_tr, x_val, y_val)
+        if p.rearrange:
+            tr_dataset_1, val_dataset_1 = rearrange_dataset(p, tr_dataset_1, val_dataset_1)
         tr_dataset_2 = load_datasets(p.train_path, p, mode=1, human_flag=p.human_flag, samp_per_id=p.samp_per_id)
         x_tr, y_tr, x_val, y_val = split_dataset(tr_dataset_2, p)
         tr_dataset_2, val_dataset_2, scaler2 = scale_dataset(p, x_tr, y_tr, x_val, y_val, mode=1)
+        if p.rearrange:
+            tr_dataset_2, val_dataset_2 = rearrange_dataset(p, tr_dataset_2, val_dataset_2, mode=1)
 
-        torch.manual_seed(p.seed)
+        # torch.manual_seed(p.seed)
 
 
-        model = TCN(1,1, layers=[128, 128, 128], ks=30)
-        model = model.to(p.device)
-        # model = Advrtset(tr_dataset_1.x.shape[1], p, ker_size=p.ker_size, stride=p.stride, pool_ker_size=p.pool_ker_size, dial=p.dial,
-        #                  drop_out=p.drop_out, num_chann=p.num_chann, num_hidden=p.num_hidden,).to(p.device)
+        # model = TCN(1,1, layers=[128, 128, 128], ks=30)
+        # model = model.to(p.device)
+        model = Advrtset(tr_dataset_1.x.shape[1], p, ker_size=p.ker_size, stride=p.stride, pool_ker_size=p.pool_ker_size, dial=p.dial,
+                         drop_out=p.drop_out, num_chann=p.num_chann, num_hidden=p.num_hidden,).to(p.device)
         if p.mult_gpu:
             model = nn.DataParallel(model, device_ids=p.device_ids)
 
@@ -91,18 +100,22 @@ if __name__ == '__main__':
 
 
     ####### TAKING FULL TRAINING SET AND RUN with TEST
-    else:
+    else:  # in p.test_mode == True, we can have either TRAINING WITHOUT VALIDATION and then test or use saved models
+        # and evaluate performance.
         if p.run_saved_models:
             print('Notice: loaded models are used')
 
-        exp_name = ['abk_min_val_loss','control_min_val_loss','both_min_val_loss','abk_max_val_acc','control_max_val_acc',
+        exp_name = ['abk_min_val_loss', 'control_min_val_loss', 'both_min_val_loss', 'abk_max_val_acc', 'control_max_val_acc',
                     'control_max_val_acc'] # for now we take only the max val_acc values and not the highest mean
         # chosen_epoch = [80, 200, 319, 274, 410, 10]
 
 
-        saved_models_path = ['/home/smorandv/ac8_and_aging_NEW/ac8_and_aging/logs/Nov-16-2022_17_57_41/',
-                             '/home/smorandv/ac8_and_aging_NEW/ac8_and_aging/logs/Nov-16-2022_18_02_17/',
-                             '/home/smorandv/ac8_and_aging_NEW/ac8_and_aging/logs/Nov-16-2022_18_07_51/']
+        # saved_models_path = ['/home/smorandv/ac8_and_aging_NEW/ac8_and_aging/logs/Nov-16-2022_17_57_41/',
+        #                      '/home/smorandv/ac8_and_aging_NEW/ac8_and_aging/logs/Nov-16-2022_18_02_17/',
+        #                      '/home/smorandv/ac8_and_aging_NEW/ac8_and_aging/logs/Nov-16-2022_18_07_51/']
+        saved_models_path = ['/home/smorandv/ac8_and_aging_NEW/ac8_and_aging/logs/Nov-23-2022_18_39_30/',
+                            '/home/smorandv/ac8_and_aging_NEW/ac8_and_aging/logs/Nov-23-2022_18_44_47/',
+                            '/home/smorandv/ac8_and_aging_NEW/ac8_and_aging/logs/Nov-23-2022_18_50_44/']
         model_type = ['final_model.pt']
         hyperparameter_dict = {'abk_min_val_loss': dict(num_epochs=80, b=-0.5928304672016774, batch_size=16, drop_out=0.33817387726694303,
                                  ker_size=30, lmbda=0.15788530065565576, lr=2.779077656310891e-07,
@@ -111,9 +124,9 @@ if __name__ == '__main__':
                                      ker_size=30, lmbda=9.613353037151509, lr=3.435639818473589e-07,
                                      med_mode='c', momentum=0.755330893385039, weight_decay=1.1752777398646077),
 
-                               'both_min_val_loss': dict(num_epochs = 319, b = 0.4770143418377288, batch_size = 128, drop_out = 0.30722983323578223,
-                                                         ker_size = 10, lmbda = 1.3150498005398192, lr = 4.078531801074058e-07,
-                                                         med_mode = 'both', momentum = 0.10744132018019596, weight_decay = 0.17254862160126153)}
+                               'both_min_val_loss': dict(num_epochs=319, b=0.4770143418377288, batch_size=128, drop_out=0.30722983323578223,
+                                                         ker_size=10, lmbda=1.3150498005398192, lr=4.078531801074058e-07,
+                                                         med_mode='both', momentum=0.10744132018019596, weight_decay=0.17254862160126153)}
 
                                # 'abk_max_val_acc': dict(num_epochs = 274, b = -0.9562638379209744, batch_size = 128, drop_out = 0.17056978300508333,
                                #                         ker_size = 10, lmbda = 3.116931692726408, lr = 4.052054605371503e-09,
@@ -138,13 +151,13 @@ if __name__ == '__main__':
             tr_dataset_1 = load_datasets(p.train_path, p, human_flag=p.human_flag, samp_per_id=p.samp_per_id)
             ts_dataset_1 = load_datasets(p.test_path, p, human_flag=p.human_flag, samp_per_id=p.samp_per_id, train_mode=False)
             tr_dataset_1, ts_dataset_1 = scale_dataset(p, tr_dataset_1, ts_dataset_1, should_scale=False)
-            # todo: notice the following line of rearranging. Consider dropping it if you don't want the whole dataset with "inproper" partition
-            # tr_dataset_1, ts_dataset_1 = rearrange_dataset(p, tr_dataset_1, ts_dataset_1)
+            if p.rearrange:
+                tr_dataset_1, ts_dataset_1 = rearrange_dataset(p, tr_dataset_1, ts_dataset_1)
             tr_dataset_2 = load_datasets(p.train_path, p, human_flag=p.human_flag, samp_per_id=p.samp_per_id, mode=1)
             ts_dataset_2 = load_datasets(p.test_path, p, human_flag=p.human_flag, samp_per_id=p.samp_per_id, train_mode=False, mode=1)
             tr_dataset_2, ts_dataset_2 = scale_dataset(p, tr_dataset_2, ts_dataset_2, mode=1, should_scale=False)
-            # todo: notice the following line of rearranging. Consider dropping it if you don't want the whole dataset with "inproper" partition
-            # tr_dataset_2, ts_dataset_2 = rearrange_dataset(p, tr_dataset_2, ts_dataset_2, mode=1)
+            if p.rearrange:
+                tr_dataset_2, ts_dataset_2 = rearrange_dataset(p, tr_dataset_2, ts_dataset_2, mode=1)
 
             acc_vector = torch.zeros(1)
             for i in range(1):
@@ -171,7 +184,8 @@ if __name__ == '__main__':
                 tsloader2 = torch.utils.data.DataLoader(
                     ts_dataset_2, batch_size=p.batch_size, shuffle=False, num_workers=0, drop_last=True)
                 if p.run_saved_models:
-                    model = torch.load(saved_models_path[idx_exp] + model_type[0])
+                    p.log_path = saved_models_path[idx_exp]
+                    model = torch.load(p.log_path + model_type[0])
                     _, _, acc_vector[i] = eval_model(model, p, 1, tsloader1, tsloader2)
                 else:
                     train_model(model, p, optimizer, trainloader1, trainloader2, tsloader1, tsloader2)